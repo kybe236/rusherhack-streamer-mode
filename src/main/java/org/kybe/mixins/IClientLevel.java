3bf004f420de1151cf11a30bea2f54a21740441e
Kyrill Belomenov <apfel236@outlook.de>
2025-02-26 21:00:11 +0100

    remove cool advertising

--- src/main/java/org/kybe/mixins/IClientLevel.java
+++ src/main/java/org/kybe/mixins/IClientLevel.java
@@ -4,7 +4,6 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 import org.kybe.StreamerModeModule;
-import org.kybe.utils.MapUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -12,22 +11,18 @@ import java.io.IOException;
 
 @Mixin(ClientLevel.class)
 public class IClientLevel {
-	int tick = 0;
-
 	@ModifyReturnValue(method = "getMapData", at = @At("RETURN"))
 	private MapItemSavedData getMapData(MapItemSavedData value) {
 		if (value == null) return null;
 		if (StreamerModeModule.INSTANCE == null || !StreamerModeModule.INSTANCE.isToggled() || !StreamerModeModule.INSTANCE.hideMapContents.getValue()) return value;
 		MapItemSavedData res = MapItemSavedData.createFresh((double)128.0, (double)128, (byte)1, false, false, value.dimension);
-		try {
-			tick++;
-			if (tick == Integer.MAX_VALUE) tick = 0;
-			res.colors = MapUtil.getMapColorsFromResource("/code-kybe.png", tick);
-			return res;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return res;
+
+		byte[] colors = new byte[16384];
+		for (int i = 0; i < 16384; i++) {
+			colors[i] = (byte)8;
 		}
+		res.colors = colors;
+		return res;
 	}
 
 
